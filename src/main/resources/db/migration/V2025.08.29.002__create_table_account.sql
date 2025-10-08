52bac07127856450d74eda1b11ddd09591aee4dd
felipemarianoferr <felipemf2@al.insper.edu.br>
2025-10-08 12:48:51 -0300

    product

--- src/main/resources/db/migration/V2025.08.29.002__create_table_account.sql
+++ src/main/resources/db/migration/V2025.08.29.002__create_table_account.sql
@@ -1,8 +1,8 @@
-CREATE TABLE account
+CREATE TABLE product
 (
-    id varchar(36) NOT NULL,
+    id varchar(36) NOT NULL AUTO_INCREMENT,
     name varchar(255) NOT NULL,
-    email varchar(255) NOT NULL,
-    sha256 varchar(64) NOT NULL,
-    CONSTRAINT account_pkey PRIMARY KEY (id)
+    price decimal(10,2) NOT NULL,
+    unit varchar(64) NOT NULL,
+    CONSTRAINT product_pkey PRIMARY KEY (id)
 )
